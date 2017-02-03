90999a0b03e760e68a1e2b427469fa11ab0d47e9
Stefan Siegl <stesie@brokenpipe.de>
2017-02-03 23:56:20 +0100

    hello world

---
+++ node_extensions.rb
@@ -0,0 +1,23 @@
+module TodoistQuery
+  class QueryExpression < Treetop::Runtime::SyntaxNode
+  end
+
+  class Union < Treetop::Runtime::SyntaxNode
+  end
+
+  class Intersection < Treetop::Runtime::SyntaxNode
+  end
+
+  class Overdue < Treetop::Runtime::SyntaxNode
+  end
+
+  class LiteralQuery < Treetop::Runtime::SyntaxNode
+  end
+
+  class NDaysQuery < Treetop::Runtime::SyntaxNode
+  end
+
+  class PriorityQuery < Treetop::Runtime::SyntaxNode
+  end
+
+end
---
+++ parser.rb
@@ -0,0 +1,29 @@
+require 'treetop'
+
+#require File.join(base_path, 'node_extensions.rb')
+require './node_extensions'
+
+class Parser
+  base_path = File.expand_path(File.dirname(__FILE__))
+  Treetop.load(File.join(base_path, 'todoist_query_parser.treetop'))
+  @@parser = TodoistQueryParser.new
+
+  def self.parse(data)
+    tree = @@parser.parse(data)
+
+    if tree.nil?
+      raise Exception, "Parse error at offset: #{@@parser.index}"
+    end
+    
+    return tree
+  end
+end
+
+def dump_tree(indent, node)
+  puts "   " * indent + node.to_s
+  return if node.elements.nil?
+  node.elements.each  {|node| dump_tree 1 + indent, node}
+end
+	 
+#puts Parser.parse("overdue")
+dump_tree 0, Parser.parse("today | today & p1 | 4 days")
