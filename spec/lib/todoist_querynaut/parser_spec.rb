59af8175f9f3fefafe0995da4b99c66dd778e677
Stefan Siegl <stesie@brokenpipe.de>
2017-02-04 21:37:36 +0100

    add specs

---
+++ spec/lib/todoist_querynaut/parser_spec.rb
@@ -0,0 +1,39 @@
+require "spec_helper"
+
+describe "#parse" do
+  describe "query literals" do
+    it "should parse 'today'" do
+      tree = TodoistQuerynaut::Parser.parse("today")
+      expect(tree).to be_a(TodoistQuerynaut::TodoistQuery::LiteralQuery)
+    end
+
+    it "should parse 'tomorrow'" do
+      tree = TodoistQuerynaut::Parser.parse("tomorrow")
+      expect(tree).to be_a(TodoistQuerynaut::TodoistQuery::LiteralQuery)
+    end
+
+    it "should parse 'overdue'" do
+      tree = TodoistQuerynaut::Parser.parse("overdue")
+      expect(tree).to be_a(TodoistQuerynaut::TodoistQuery::LiteralQuery)
+    end
+
+    it "should parse 'over due'" do
+      tree = TodoistQuerynaut::Parser.parse("over due")
+      expect(tree).to be_a(TodoistQuerynaut::TodoistQuery::LiteralQuery)
+    end
+
+    it "should parse 'view all'" do
+      tree = TodoistQuerynaut::Parser.parse("view all")
+      expect(tree).to be_a(TodoistQuerynaut::TodoistQuery::LiteralQuery)
+    end
+  end
+
+  describe "negated queries" do
+    it "should parse '!today'" do
+      tree = TodoistQuerynaut::Parser.parse("!today")
+      expect(tree).to be_a(TodoistQuerynaut::TodoistQuery::NegatedQuery)
+      expect(tree.elements[0]).to be_a(TodoistQuerynaut::TodoistQuery::LiteralQuery)
+    end
+  end
+
+end
---
+++ spec/spec_helper.rb
@@ -0,0 +1,1 @@
+require "todoist_querynaut"
