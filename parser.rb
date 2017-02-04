f80936da18351675e2721e798314df02b472a451
Stefan Siegl <stesie@brokenpipe.de>
2017-02-04 20:18:07 +0100

    refine

--- parser.rb
+++ parser.rb
@@ -6,6 +6,7 @@ require './node_extensions'
 class Parser
   base_path = File.expand_path(File.dirname(__FILE__))
   Treetop.load(File.join(base_path, 'todoist_query_parser.treetop'))
+  #require './todoist_query_parser.rb'
   @@parser = TodoistQueryParser.new
 
   def self.parse(data)
@@ -14,8 +15,26 @@ class Parser
     if tree.nil?
       raise Exception, "Parse error at offset: #{@@parser.index}"
     end
-    
-    return tree
+
+    #return tree
+    return self.clean_tree(tree)
+  end
+
+  private
+
+  def self.clean_tree(node)
+    if node.class.name == "Treetop::Runtime::SyntaxNode"
+      if node.elements.nil? || node.elements.length == 0
+	return []
+      else
+        return node.elements.map{|n| self.clean_tree n}.flatten(1)
+      end
+    else
+      if !node.elements.nil?
+	node.elements.replace node.elements.map{|n| self.clean_tree n}.flatten(1)
+      end
+      return node
+    end
   end
 end
 
@@ -24,9 +43,15 @@ def dump_tree(indent, node)
   return if node.elements.nil?
   node.elements.each  {|node| dump_tree 1 + indent, node}
 end
-	 
+
 #puts Parser.parse("overdue")
 #dump_tree 0, Parser.parse("today | today & p1 | 4 days")
 #dump_tree 0, Parser.parse("@foobar")
 #dump_tree 0, Parser.parse("no labels")
-dump_tree 0, Parser.parse("7 days & !@home")
+#dump_tree 0, Parser.parse("(overdue) | today")
+dump_tree 0, Parser.parse("7 days & @foo | today")
+#dump_tree 0, Parser.parse("7 days & 4 days & !@home")
+
+#puts Parser.parse("7 days & !@home").to_s
+
+#puts Parser.clean_tree( Parser.parse("7 days & 4 days & !@home") )
