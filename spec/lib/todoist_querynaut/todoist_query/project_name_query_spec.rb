26c84b60cdf469e9e5fa28377ea1ead1d74f939d
Stefan Siegl <stesie@brokenpipe.de>
2017-03-03 21:36:15 +0100

    extract fixture into before :each

--- spec/lib/todoist_querynaut/todoist_query/project_name_query_spec.rb
+++ spec/lib/todoist_querynaut/todoist_query/project_name_query_spec.rb
@@ -14,15 +14,17 @@ describe TodoistQuerynaut::TodoistQuery::ProjectNameQuery do
   end
 
   describe "#run_query" do
-    it "should run a 'p:Inbox' query" do
-      project_name_query = TodoistQuerynaut::TodoistQuery::ProjectNameQuery.new("p:Inbox", 0...7)
-
+    before :each do
       stub_request(:post, "https://todoist.com/API/v6/query").
         with(:body => {"queries" => "[\"view all\"]", "token" => "some_token"}).
         to_return(:status => 200, :body => json_response_raw("query_view_all"), :headers => {})
       stub_request(:post, "https://todoist.com/API/v6/sync").
         with(:body => { "seq_no" => "0", "seq_no_global" => "0", "resource_types" => '["projects"]', "token" => "some_token" }).
         to_return(:status => 200, :body => json_response_raw("sync_projects_all"), :headers => {})
+    end
+
+    it "should run a 'p:Inbox' query" do
+      project_name_query = TodoistQuerynaut::TodoistQuery::ProjectNameQuery.new("p:Inbox", 0...7)
       result = project_name_query.run_query TodoistQuerynaut::Client.new(Todoist::Client.new("some_token"))
 
       expect(result.size).to eq(1)
